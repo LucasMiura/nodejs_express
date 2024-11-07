d9c48170075574988b8ad3b35e0bfe240dc32c24
DSM2 <fatec-dsm2@academico.intra>
2024-11-07 19:53:03 -0300

    @LucasMiura

---
+++ aula-06-sistema-de-login/controllers/PedidosController.js
@@ -0,0 +1,80 @@
+import express, { Router } from "express";
+const router = express.Router();
+// Importando o model de Pedido
+import Pedido from "../models/Pedido.js";
+//Importando o Middleware Auth
+import Auth from "../middleware/Auth.js";
+
+// ROTA PEDIDOS
+router.get("/pedidos", Auth, (req, res) => {
+  Pedido.findAll().then((pedidos) => {
+    res.render("pedidos", {
+      pedidos: pedidos,
+    });
+  });
+});
+
+//ROTA DE CADASTRO DE PEDIDOS
+router.post("/pedidos/new", Auth, (req, res) => {
+  // RECEBENDO OS DADOS DO FORMULÁRIO E GRAVANDO NAS VARIÁVEIS
+  const numero = req.body.numero
+  const valor = req.body.valor
+  Pedido.create({
+    numero: numero,
+    valor: valor,
+  }).then(() => {
+    res.redirect("/pedidos")
+  })
+});
+
+// ROTA DE EXCLUSÃO DE CLIENTE
+// ESSA ROTA POSSUI UM PARÂMETRO ID
+router.get("/pedidos/delete/:id", Auth, (req, res) => {
+  // COLETAR O ID QUE VEIO NA URL
+  const id = req.params.id;
+  // MÉTODO PARA EXCLUIR
+  Pedido.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/pedidos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE EDIÇÃO DE PEDIDO
+router.get("/pedidos/edit/:id", Auth, (req, res) => {
+  const id = req.params.id;
+  Pedido.findByPk(id)
+    .then((pedido) => {
+      res.render("pedidoEdit", {
+        pedido: pedido,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE ALTERAÇÃO DE PEDIDO
+router.post("/pedidos/update", Auth, (req, res) => {
+  const id = req.body.id;
+  const numero = req.body.numero;
+  const valor = req.body.valor;
+  Pedido.update({
+    numero: numero,
+    valor: valor,
+  },
+  {where: {id: id}}
+).then(() => {
+  res.redirect("/pedidos")
+}).catch((error) => {
+  console.log(error)
+})
+});
+
+export default router;
---
+++ aula-06-sistema-de-login/controllers/ProdutosController.js
@@ -0,0 +1,84 @@
+import express, { Router } from "express";
+const router = express.Router();
+// Importando o model de Produto
+import Produto from "../models/Produto.js";
+//Importando o Middleware Auth
+import Auth from "../middleware/Auth.js";
+
+// ROTA PRODUTOS
+router.get("/produtos", Auth, (req, res) => {
+  Produto.findAll().then((produtos) => {
+    res.render("produtos", {
+      produtos: produtos,
+    });
+  });
+});
+
+// ROTA DE CADASTRO DE PRODUTOS
+router.post("/produtos/new", Auth, (req, res) => {
+  // RECEBENDO OS DADOS DO FORMULÁRIO E GRAVANDO NAS VARIÁVEIS
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+  Produto.create({
+    nome: nome,
+    preco: preco,
+    categoria: categoria,
+  }).then(() => {
+    res.redirect("/produtos");
+  });
+});
+
+// ROTA DE EXCLUSÃO DE PRODUTO
+// ESSA ROTA POSSUI UM PARÂMETRO ID
+router.get("/produtos/delete/:id", Auth, (req, res) => {
+  // COLETAR O ID QUE VEIO NA URL
+  const id = req.params.id;
+  // MÉTODO PARA EXCLUIR
+  Produto.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE EDIÇÃO DE PRODUTO
+router.get("/produtos/edit/:id", Auth, (req, res) => {
+  const id = req.params.id;
+  Produto.findByPk(id)
+    .then((produto) => {
+      res.render("produtoEdit", {
+        produto: produto,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE ALTERAÇÃO DE PRODUTO
+router.post("/produtos/update", Auth, (req, res) => {
+  const id = req.body.id;
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+  Produto.update({
+    nome: nome,
+    preco: preco,
+    categoria: categoria,
+  },
+  {where: {id: id}}
+).then(() => {
+  res.redirect("/produtos")
+}).catch((error) => {
+  console.log(error)
+})
+});
+
+export default router;
